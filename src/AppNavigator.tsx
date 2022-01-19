003e41b705f1211e5d354e790b35036d69e3294d
Isaac Sherrill <isaac@famapp.com>
2022-01-19 14:45:03 -0500

    uninstall some things

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
 import {GameView} from './views/GameView';
-import {GameDrawer} from './views/GameDrawer';
 
 export function AppNavigator(): JSX.Element {
-  return (
-    <NavigationContainer>
-      <GameView />
-    </NavigationContainer>
-  );
+  return <GameView />;
 }
