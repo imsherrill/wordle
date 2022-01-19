2c540021d3e6386358a78d24dd9833cc62cf2d18
Isaac Sherrill <isaac@famapp.com>
2022-01-19 15:19:51 -0500

    basic drawer working

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {GameView} from './views/GameView';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Button, Text, View} from 'react-native';
 
-const Stack = createNativeStackNavigator();
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {GameDrawer} from './views/GameDrawer';
+
+const Drawer = createDrawerNavigator();
 
 export function AppNavigator(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Game" component={GameView} />
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Game" component={GameView} />
+      </Drawer.Navigator>
     </NavigationContainer>
-  );
-}
-
-function HomeScreen({navigation}): JSX.Element {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button title="Go to Game" onPress={() => navigation.navigate('Game')} />
-    </View>
   );
 }
