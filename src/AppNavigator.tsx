55d4b8a5b910923d9cba50e901b4213e7a71e437
Isaac Sherrill <isaac@famapp.com>
2022-01-19 14:52:06 -0500

    simple navigation working

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
-
+import {NavigationContainer} from '@react-navigation/native';
 import {GameView} from './views/GameView';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Button, Text, View} from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export function AppNavigator(): JSX.Element {
-  return <GameView />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Game" component={GameView} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({navigation}): JSX.Element {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button title="Go to Game" onPress={() => navigation.navigate('Game')} />
+    </View>
+  );
 }
