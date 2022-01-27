c84fcb94740257a342d2216816d61eb304ba3282
Isaac Sherrill <isaac@famapp.com>
2022-01-26 21:41:23 -0500

    ignore logbox;

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 
 import 'react-native-gesture-handler';
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+LogBox.ignoreLogs(['react-native-gesture-handler']); // React Navigation's Drawer Navigator hasn't yet upgraded to gesture handler > 2.1.x
 
 AppRegistry.registerComponent(appName, () => App);
